c1de0e80d0354d27741ed37c5a6b71aca9268ca6
Simona Ričovská <simonaricovska@gmail.com>
2026-02-10 20:19:22 +0100

    made adapter, but not fully finctional. something wrong

--- 26.02.10_Adapter.py
+++ 26.02.10_Adapter.py
@@ -1,5 +1,26 @@
 
 # STRUCTURAL
 
+class Legacy:
+    def legacy_method(self):
+        return "legacy method"
 
 
+
+class NewApp:
+    def new_method(self):
+        pass
+
+
+
+class Adapter(NewApp):
+    def __init__(self):
+        self.legacy = Legacy()
+
+    def new_method(self):
+        return self.legacy.legacy_method()
+
+
+
+    newApp = Adapter()
+    print(newApp.new_method())
