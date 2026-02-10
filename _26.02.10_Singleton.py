799ce2718711429736695a654a9f18c94a83e265
Simona Ričovská <simonaricovska@gmail.com>
2026-02-10 19:05:59 +0100

    the base done

--- _26.02.10_Singleton.py
+++ _26.02.10_Singleton.py
@@ -1,3 +1,21 @@
 
 
+class Singleton:
+    #atribute of singleton, exists only one such object
+    object = None
+
+    @staticmethod
+    def getInstance():
+        if Singleton.object is None:
+            Singleton.object = Singleton()
+        return Singleton.object
+
+
+
+
+object1 = Singleton().getInstance()
+print(object1)
+
+object2 = Singleton().getInstance()
+print(object2)
 
